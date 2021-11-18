1ec4f3164e6c0723484b0c4a111224d54d948027
purplefeetguy <purplefeetguy>
2021-11-18 04:59:38 -1000

    added changes to main.tf

--- 2.My-Stuff/main.tf
+++ 2.My-Stuff/main.tf
@@ -1,24 +1,6 @@
 resource "azure_resource_group" "azure_k8s" {
-  name     = local.common_name
+  name     = "${local.common_name}-rg"
   location = var.location
   tags     = var.tag
 }
 
-resource "azure_log_analytics_workspace" "azure_law" {
-  name                = "pfg-workspace"
-  resource_group_name = azure_resource_group.azure_k8s.name
-  sku                 = "Free"
-  retention_in_days   = 7
-}
-
-resource "azure_log_analytics_solution" "azure_las" {
-  solution_name         = "ContainerInsights"
-  resource_group_name   = azure_resource_group.azure_k8s.name
-  location              = var.location
-  workspace_name        = azure_law.name
-  workspace_resource_id = azure_log_analytics_workspace.azure_k8s.id
-  plan {
-    publisher = "Microsoft"
-    product   = "OMSGallery/ContainerInsights"
-  }
-}
